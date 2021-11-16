67c165ce0fc3d315cc2765354f66f60186fe7c60
kirill <rroogg76aacchh@gmail.com>
2021-11-16 18:05:59 +0300

    Added Cuda main and plan

---
+++ main.cu
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    // Get triangles from file
+    // Sort vertexes of triangles to get a web
+    // Sort triangles for each slice, delete all others vertexes and triangles
+    // For each triangle get it vertexes and lines crossing with slice
+    // Make chains from lines and vertexes
+    // Print every chain on array
+    // Save array as green color to png
+    return 0;
+}
